a1a8427964b1ceca8faa2e3f211a6851728b7f56
Marouéne <tayari_marouene@yahoo.fr>
2015-10-29 16:05:53 +0100

    Signed-off-by: Marouéne <tayari_marouene@yahoo.fr>

---
+++ greeter/src/main/java/edu/esprit/greeter/Greeter.java
@@ -0,0 +1,21 @@
+package edu.esprit.greeter;
+
+public class Greeter {
+	/**
+	 * 
+	 * @param name
+	 * @return
+	 */
+	//Test1
+	//Test2
+	//Test3
+	//Test465
+	//test25
+	//last test
+public String sayHello(String name)
+{
+	if(name.equals("MAN"))
+			throw new IllegalArgumentException();
+			return "Hello "+name;
+	}
+}
